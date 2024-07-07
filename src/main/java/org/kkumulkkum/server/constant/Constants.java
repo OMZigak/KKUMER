978686c3b9d4335f523accd5caee80b60e8ffa35
chaewonkim <kimlove9182@gmail.com>
2024-07-08 05:32:50 +0900

    [feat] #9 create Constants class to manage constant in one class

---
+++ src/main/java/org/kkumulkkum/server/constant/Constants.java
@@ -0,0 +1,8 @@
+package org.kkumulkkum.server.constant;
+
+public class Constants {
+
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String BEARER = "Bearer ";
+    public static final String CHARACTER_TYPE = "utf-8";
+}
