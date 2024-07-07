daba8e49c471740c4250fdd9dd239cc19308b465
chaewonni <113420297+chaewonni@users.noreply.github.com>
2024-07-08 00:18:06 +0900

    [feat] #12 공통 응답 형태 클라 요청 반영 (#13)

--- src/main/java/org/kkumulkkum/server/dto/common/ResponseDto.java
+++ src/main/java/org/kkumulkkum/server/dto/common/ResponseDto.java
@@ -3,14 +3,15 @@ package org.kkumulkkum.server.dto.common;
 import org.kkumulkkum.server.exception.code.DefaultErrorCode;
 
 public record ResponseDto<T> (
+        boolean success,
         T data,
         ErrorDto error
 ) {
     public static <T> ResponseDto<T> success(final T data) {
-        return new ResponseDto<>(data, null);
+        return new ResponseDto<>(true, data, null);
     }
 
     public static <T> ResponseDto<T> fail(DefaultErrorCode error) {
-        return new ResponseDto<>(null, ErrorDto.of(error.getCode(), error.getMessage()));
+        return new ResponseDto<>(false, null, ErrorDto.of(error.getCode(), error.getMessage()));
     }
 }
