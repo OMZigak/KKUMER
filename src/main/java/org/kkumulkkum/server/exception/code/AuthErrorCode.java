a1c8b87f29a745bfb6791ba0ee1f21adef1d4d23
Sangjune park <74230343+tkdwns414@users.noreply.github.com>
2024-07-08 23:50:25 +0900

    [fix] Jwt 예외 처리 추가 및 TestToken 발급 추가 (#18)
    * [fix] #17 handle condition of jwt when token is null
    
    * [fix] #17 create TeskToken API
    
    * [fix] #17 add error code in AuthErrorCode
    
    * [fix] #17 create & add jwtExceptionFilter
    
    * [fix] #17 add logic to handle Exception in jwt Filter
    
    * [chore] #17 add jwt settings in yml

--- src/main/java/org/kkumulkkum/server/exception/code/AuthErrorCode.java
+++ src/main/java/org/kkumulkkum/server/exception/code/AuthErrorCode.java
@@ -8,12 +8,13 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum AuthErrorCode implements DefaultErrorCode {
 
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, 40191, "인증되지 않은 사용자입니다."),
-    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, 40192, "액세스 토큰의 형식이 올바르지 않습니다."),
-    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, 40193, "액세스 토큰이 만료되었습니다."),
-    UNSUPPORTED_TOKEN(HttpStatus.UNAUTHORIZED, 40194, "지원하지 않는 토큰 형식입니다."),
-    EMPTY_TOKEN(HttpStatus.UNAUTHORIZED, 40195, "토큰이 제공되지 않았습니다."),
-    MISMATCH_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, 40196, "리프레시 토큰이 일치하지 않습니다."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, 40190, "인증되지 않은 사용자입니다."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, 40191, "액세스 토큰의 형식이 올바르지 않습니다."),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, 40192, "액세스 토큰이 만료되었습니다."),
+    UNSUPPORTED_TOKEN(HttpStatus.UNAUTHORIZED, 40193, "지원하지 않는 토큰 형식입니다."),
+    EMPTY_TOKEN(HttpStatus.UNAUTHORIZED, 40194, "토큰이 제공되지 않았습니다."),
+    MISMATCH_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, 40195, "리프레시 토큰이 일치하지 않습니다."),
+    UNKNOWN_TOKEN(HttpStatus.UNAUTHORIZED, 40196, "알 수 없는 토큰입니다."),
     ;
 
     private HttpStatus httpStatus;
