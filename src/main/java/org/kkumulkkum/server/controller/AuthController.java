accf377bf4bad3dc569e20bd7468f5c27f6eb9bd
chaewonkim <kimlove9182@gmail.com>
2024-07-09 04:05:50 +0900

    [feat] #10 create user signin logic

---
+++ src/main/java/org/kkumulkkum/server/controller/AuthController.java
@@ -0,0 +1,28 @@
+package org.kkumulkkum.server.controller;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.kkumulkkum.server.constant.Constant;
+import org.kkumulkkum.server.dto.auth.request.UserLoginDto;
+import org.kkumulkkum.server.dto.auth.response.JwtTokenDto;
+import org.kkumulkkum.server.service.auth.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/auth/signin")
+    public ResponseEntity<JwtTokenDto> signin(
+            @NotNull @RequestHeader(Constant.AUTHORIZATION_HEADER) final String providerToken,
+            @Valid @RequestBody UserLoginDto userLoginDto
+    ) {
+        return ResponseEntity.ok(authService.signin(providerToken, userLoginDto));
+    }
+
+}
