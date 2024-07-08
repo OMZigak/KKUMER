733f35ecbab83b173ad26238b688331467450211
chaewonkim <kimlove9182@gmail.com>
2024-07-09 04:04:48 +0900

    [feat] #10 add not_found_user exception

---
+++ src/main/java/org/kkumulkkum/server/exception/code/UserErrorCode.java
@@ -0,0 +1,17 @@
+package org.kkumulkkum.server.exception.code;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum UserErrorCode implements DefaultErrorCode {
+    // 404 Not Found
+    NOT_FOUND_USER(HttpStatus.NOT_FOUND, 40410, "유저를 찾을 수 없습니다."),
+    ;
+
+    private HttpStatus httpStatus;
+    private int code;
+    private String message;
+}
