2c7a2eea5d7ba3eda62b50f35c1737ca2c6b52e4
tkdwns414 <sangjune2000@naver.com>
2024-07-09 01:02:14 +0900

    [fix] #16 add Validation Exception in error handler

--- src/main/java/org/kkumulkkum/server/exception/code/BusinessErrorCode.java
+++ src/main/java/org/kkumulkkum/server/exception/code/BusinessErrorCode.java
@@ -7,8 +7,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum BusinessErrorCode implements DefaultErrorCode {
-
+    // 400 Bad Request
     BAD_REQUEST(HttpStatus.BAD_REQUEST,40000, "잘못된 요청입니다."),
+    INVALID_ARGUMENTS(HttpStatus.BAD_REQUEST, 40001, "인자의 형식이 올바르지 않습니다."),
+    // 404 Not Found
     NOT_FOUND(HttpStatus.NOT_FOUND,40400, "요청한 정보를 찾을 수 없습니다."),
     NOT_FOUND_END_POINT(HttpStatus.NOT_FOUND,40401, "요청한 엔드포인트를 찾을 수 없습니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,50000, "서버 내부 오류입니다."),
