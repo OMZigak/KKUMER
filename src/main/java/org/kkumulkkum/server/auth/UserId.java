6f063be5a3fe66c40da08da16323804a327a8467
chaewonkim <kimlove9182@gmail.com>
2024-07-08 18:36:03 +0900

    [feat] #9 add AuthenticationPrincipal annotation

--- src/main/java/org/kkumulkkum/server/auth/UserId.java
+++ src/main/java/org/kkumulkkum/server/auth/UserId.java
@@ -1,5 +1,7 @@
 package org.kkumulkkum.server.auth;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -7,5 +9,6 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
+@AuthenticationPrincipal(expression="T(org.kkumulkkum.server.auth.jwt.JwtTokenProvider).checkPrincipal(#this)")
 public @interface UserId {
 }
