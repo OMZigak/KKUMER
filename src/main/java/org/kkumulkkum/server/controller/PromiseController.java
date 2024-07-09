55b64258967a97de61f169622144931551001f69
tkdwns414 <sangjune2000@naver.com>
2024-07-09 21:18:46 +0900

    [feat] #24 mv APIs to appropriate controller

--- src/main/java/org/kkumulkkum/server/controller/PromiseController.java
+++ src/main/java/org/kkumulkkum/server/controller/PromiseController.java
@@ -1,11 +1,6 @@
 package org.kkumulkkum.server.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.kkumulkkum.server.annotation.UserId;
-import org.kkumulkkum.server.service.participant.ParticipantService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,34 +8,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class PromiseController {
-
-    private final ParticipantService participantService;
-
-    @PatchMapping("/promises/{promiseId}/preperation")
-    public ResponseEntity<Void> preparePromise(
-            @UserId Long userId,
-            @PathVariable Long promiseId
-    ) {
-        participantService.preparePromise(userId, promiseId);
-        return ResponseEntity.ok().build();
-    }
-
-    @PatchMapping("/promises/{promiseId}/departure")
-    public ResponseEntity<Void> departurePromise(
-            @UserId Long userId,
-            @PathVariable Long promiseId
-    ) {
-        participantService.departurePromise(userId, promiseId);
-        return ResponseEntity.ok().build();
-    }
-
-    @PatchMapping("/promises/{promiseId}/arrival")
-    public ResponseEntity<Void> arrivalPromise(
-            @UserId Long userId,
-            @PathVariable Long promiseId
-    ) {
-        participantService.arrivalPromise(userId, promiseId);
-        return ResponseEntity.ok().build();
-    }
 
 }
