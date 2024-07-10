d5433f49e75bf7c94bb552a3a877217e1219c508
chaewonkim <kimlove9182@gmail.com>
2024-07-10 17:01:01 +0900

    [fix] #28 delete unnecessary query

--- src/main/java/org/kkumulkkum/server/repository/PromiseRepository.java
+++ src/main/java/org/kkumulkkum/server/repository/PromiseRepository.java
@@ -2,12 +2,10 @@ package org.kkumulkkum.server.repository;
 
 import org.kkumulkkum.server.domain.Promise;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface PromiseRepository extends JpaRepository<Promise, Long> {
 
-    @Query("SELECT p FROM Promise p JOIN p.meeting m WHERE m.id = :meetingId")
     List<Promise> findAllByMeetingId(Long meetingId);
 }
