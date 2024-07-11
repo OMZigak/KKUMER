3560ed02b78d7e9dd50f91a291b0996f93f20fc0
chaewonni <113420297+chaewonni@users.noreply.github.com>
2024-07-12 07:30:37 +0900

    [feat] 약속 지각 상세 조회 API 추가 작업 (#43)
    * [feat] #39 modify query to consider participant with null arrivalAt
    
    * [feat] #39 remove unnecessary statement
    
    ---------
    
    Co-authored-by: Sangjune park <74230343+tkdwns414@users.noreply.github.com>

--- src/main/java/org/kkumulkkum/server/service/participant/ParticipantService.java
+++ src/main/java/org/kkumulkkum/server/service/participant/ParticipantService.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -79,6 +80,11 @@ public class ParticipantService {
     @Transactional(readOnly = true)
     public LateComersDto getLateComers(final Long promiseId) {
         Promise promise = promiseRetriever.findById(promiseId);
+        
+        if (promise.getTime().isAfter(LocalDateTime.now())) {
+            return LateComersDto.of(promise, Collections.emptyList());
+        }
+
         List<LateComerDto> lateComers = participantRetriever.findAllLateComersByPromiseId(promiseId);
         return LateComersDto.of(
                 promise,
