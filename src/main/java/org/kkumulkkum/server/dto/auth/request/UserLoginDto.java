59fc171c006f66b55727ebc24fd9d1813a21eeae
chaewonkim <kimlove9182@gmail.com>
2024-07-15 05:49:26 +0900

    [hotfix] change fcmToken to nullable

--- src/main/java/org/kkumulkkum/server/dto/auth/request/UserLoginDto.java
+++ src/main/java/org/kkumulkkum/server/dto/auth/request/UserLoginDto.java
@@ -1,12 +1,13 @@
 package org.kkumulkkum.server.dto.auth.request;
 
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotNull;
 import org.kkumulkkum.server.domain.enums.Provider;
 
 public record UserLoginDto(
         @NotNull
         Provider provider,
-        @NotNull
+        @Nullable
         String fcmToken
 ) {
 }
