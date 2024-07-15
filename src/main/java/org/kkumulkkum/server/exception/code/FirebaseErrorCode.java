47a08ff45e28f722cee206d82a278ffbe0986e2a
chaewonkim <kimlove9182@gmail.com>
2024-07-16 04:04:56 +0900

    [fix] #56 edit error code number

--- src/main/java/org/kkumulkkum/server/exception/code/FirebaseErrorCode.java
+++ src/main/java/org/kkumulkkum/server/exception/code/FirebaseErrorCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum FirebaseErrorCode implements DefaultErrorCode {
     // 400 BAD_REQUEST
-    FCM_ERROR(HttpStatus.BAD_REQUEST,40005,"fcm 토큰 오류입니다."),
+    FCM_ERROR(HttpStatus.BAD_REQUEST,40070,"fcm 토큰 오류입니다."),
     // 404 NOT_FOUND
     NOT_FOUND_FIREBASE_JSON(HttpStatus.NOT_FOUND, 40470, "FIREBASE JSON을 찾을 수 없습니다."),
     ;
