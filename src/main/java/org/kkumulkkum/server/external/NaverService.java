01f2b8f1a64aeba05e7209b265346c934ca4da41
tkdwns414 <sangjune2000@naver.com>
2024-07-16 06:11:26 +0900

    [fix] #60 use appropriate method to return empty List

--- src/main/java/org/kkumulkkum/server/external/NaverService.java
+++ src/main/java/org/kkumulkkum/server/external/NaverService.java
@@ -14,6 +14,7 @@ import org.springframework.web.client.RestClient;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -35,7 +36,7 @@ public class NaverService {
             NaverLocationResponse naverLocationResponse = locationSearch(requestMap(query));
             return convertResponse(naverLocationResponse);
         } catch (OpenApiException e) {
-            return List.of();
+            return Collections.emptyList();
         }
     }
 
