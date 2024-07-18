63c2d9548a80ddeb1cef89340aad0aa887b438a7
chaewonkim <kimlove9182@gmail.com>
2024-07-18 09:27:59 +0900

    [fix] #65 delete unnecessary import

--- src/main/java/org/kkumulkkum/server/controller/PromiseController.java
+++ src/main/java/org/kkumulkkum/server/controller/PromiseController.java
@@ -16,8 +16,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
-
 @RestController
 @RequiredArgsConstructor
 public class PromiseController {
